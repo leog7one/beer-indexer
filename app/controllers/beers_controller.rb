adaab040dedc5812429b9e73084f7509d925a4dd
leog7one <lguzman@me.com>
2017-08-15 01:43:42 -0500

    added edit and delete beers, issue with breweries databse going back one on beers entered

--- app/controllers/beers_controller.rb
+++ app/controllers/beers_controller.rb
@@ -3,6 +3,7 @@ class BeersController < ApplicationController
   # GET: /beers
   get "/beers" do
     @beers = Beer.all
+    @brewery = Brewery.all
     erb :"/beers/index.html"
   end
 
@@ -21,7 +22,7 @@ class BeersController < ApplicationController
       redirect '/beers/new'
     else
       @beer = current_user(session).beers.create(name: params[:name], style: params[:style], abv: params[:abv], description: params[:description])
-      @beer.brewery = Brewery.find_or_create_by(name: params[:brewery])
+      @brewery = Brewery.find_or_create_by(name: params[:brewery])
     redirect "/beers"
   end
   end
@@ -32,23 +33,53 @@ class BeersController < ApplicationController
       @beer = Beer.find_by_id(params[:id])
       @brewery = Brewery.find_by_id(params[:id])
     erb :"/beers/show.html"
-  else
+    else
     redirect '/'
-  end
+    end
   end
 
   # GET: /beers/5/edit
   get "/beers/:id/edit" do
-    erb :"/beers/edit.html"
+    if logged_in?(session)
+      @beer = Beer.find_by_id(params[:id])
+      @brewery = Brewery.find_by_id(params[:id])
+      if @beer.user_id == current_user(session).id
+        erb :"/beers/edit.html"
+      else
+       redirect '/beers'
+      end
+    else
+  redirect '/'
+  end
   end
 
   # PATCH: /beers/5
   patch "/beers/:id" do
-    redirect "/beers/:id"
+    if params[:name] == "" || params[:brewery] == "" || params[:style] == "" || params[:abv] == "" || params[:description] == ""
+    redirect "/beers/#{params[:id]}/edit"
+  else
+    @beer = Beer.find_by_id(params[:id])
+    @brewery = Brewery.find_by_id(params[:id])
+    @beer.name = params[:name]
+    @brewery.name = params[:brewery]
+    @beer.abv = params[:abv]
+    @beer.style = params[:style]
+    @beer.description = params[:description]
+    @beer.save
+    @brewery.save
+    redirect "/beers/#{@beer.id}"
+  end
   end
 
   # DELETE: /beers/5/delete
-  delete "/beers/:id/delete" do
-    redirect "/beers"
+  delete "/beers/:id" do
+    if logged_in?(session)
+      @beer = Beer.find_by_id(params[:id])
+      if @beer.user_id == current_user(session).id
+        @beer.destroy
+        redirect '/beers'
+      end
+    redirect "/"
+  end
   end
 end
